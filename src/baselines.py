99e45721430d8dee0e1c7664fc8c0ac2b97d39a1
rmshin <50217797+rmshin@users.noreply.github.com>
2024-01-06 17:08:07 +1300

    generate baseline llm-only programs for all human eval problems

--- src/baselines.py
+++ src/baselines.py
@@ -6,35 +6,32 @@ from human_eval.data import HUMAN_EVAL, stream_jsonl, write_jsonl
 problems = list(stream_jsonl(HUMAN_EVAL))
 executor = ThreadPoolExecutor(max_workers=5)
 
-## TODO: should be returning fraction of test cases passed, instead of 0/1
-def execute(problem, completion, timeout):
-    future = executor.submit(check_correctness, problem, completion, timeout)
-    result = future.result()
-    return result
-
 
 def stats_execute(problem, completion, timeout):
-    pre_base_str, tests = problem['test'].split('def check(candidate):\n')
+    pre_base_str, tests = problem["test"].split("def check(candidate):\n")
     base_str = "def check(candidate):\n"
-    split_tests = [pre_base_str + base_str + i for i in tests.split('\n') if i != '']
-    
+    split_tests = [pre_base_str + base_str + i for i in tests.split("\n") if i != ""]
+
     _problem = problem.copy()
     results = []
     for i in split_tests:
-        _problem['test'] = i
+        _problem["test"] = i
         future = executor.submit(check_correctness, _problem, completion, timeout)
         result = future.result()
         results.append(result)
-    
-    
-    return {'task_id': problem['task_id'], 'pass_rate': sum([i['passed'] for i in results])/len(results)}
+
+    return {
+        "task_id": problem["task_id"],
+        "pass_rate": sum([i["passed"] for i in results]) / len(results),
+    }
+
 
 @outlines.prompt
 def few_shots(instructions, examples, question):
     """{{ instructions }}
 
     {% for example in examples %}
-    
+
     Question:
     ```
     {{ example.prompt }}
@@ -53,6 +50,7 @@ def few_shots(instructions, examples, question):
     ```
     """
 
+
 instructions = "Please answer the following question following the examples. Generate valid python code by indenting 4 spaces always."
 examples = problems[:2]
 
@@ -60,22 +58,21 @@ from llama_cpp import Llama
 from tqdm import tqdm
 
 model = Llama(
-    model_path="../deepseek-coder-6.7b-instruct.Q5_K_M.gguf",
+    model_path="deepseek-coder-6.7b-instruct.Q5_K_M.gguf",
     n_gpu_layers=-1,
     n_ctx=2048,
     n_batch=256,
     n_threads=8,
-    logits_all=True,
 )
 
-N_SAMPLES = 20
+N_SAMPLES = 5
 samples = []
-for problem in tqdm(problems[2:20]):
+for problem in tqdm(problems[2:]):
+    prompt = few_shots(instructions, examples, problem["prompt"])
     for _ in tqdm(range(N_SAMPLES)):
-        prompt = few_shots(instructions, examples, problem['prompt'])
-        output = model(prompt=prompt, max_tokens=80, temperature=0.2, stop=['```'])
-        res = output['choices'][0]['text']
-        item = dict(task_id=problem['task_id'], completion=res)
+        output = model(prompt=prompt, max_tokens=80, temperature=0.2, stop=["```"])
+        res = output["choices"][0]["text"]
+        item = dict(task_id=problem["task_id"], completion=res)
         samples.append(item)
 
 write_jsonl("few_shot_baselines.jsonl", samples)
