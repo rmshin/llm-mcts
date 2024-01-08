540ebc9243a16532de3bbed60ab12b45300515fc
rmshin <50217797+rmshin@users.noreply.github.com>
2024-01-08 21:02:18 +1300

    generate new baselines with top-3 sampling, 256 token limit & 20 samples

--- src/baselines.py
+++ src/baselines.py
@@ -11,18 +11,19 @@ model = Llama(
     n_threads=8,
 )
 
-N_SAMPLES = 5
-samples = []
+N_SAMPLES = 20
 prompts_ids = get_prompts_with_ids()
 for prompt, task_id in tqdm(prompts_ids):
+    samples = []
     # TODO: update to generate in parallel once https://github.com/abetlen/llama-cpp-python/pull/951 lands.
     for _ in tqdm(range(N_SAMPLES)):
-        output = model(prompt=prompt, max_tokens=256, temperature=0.2, stop=["```"])
+        output = model(
+            prompt=prompt, max_tokens=256, temperature=1, top_k=3, stop=["```"]
+        )
         res = output["choices"][0]["text"]
         item = dict(task_id=task_id, completion=res)
         samples.append(item)
-
-write_jsonl("few_shot_baselines.jsonl", samples)
+    write_jsonl("few_shot_baselines_256_top_3.jsonl", samples, append=True)
 
 # run this to get the pass@k metrics
 # evaluate_functional_correctness samples.jsonl
