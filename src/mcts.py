2654374ef349fbabb57df0fcbe01bd00583b917a
rmshin <50217797+rmshin@users.noreply.github.com>
2024-01-29 16:15:25 +1300

    web: capture tree data for web visualisation + implement basic html dag renderer

--- src/mcts.py
+++ src/mcts.py
@@ -2,6 +2,7 @@ from visualise import render_graphviz_tree
 from llama_cpp import Llama
 from math import exp, log, inf, sqrt
 import time, sys
+import itertools, json, copy
 
 
 def main():
@@ -29,13 +30,15 @@ def main():
     )
 
     max_rollouts = 128
-    top_k = 5
+    top_k = 3
     beam_width = 1
     # hyperparameters for P-UCB function
     c_base = 10
     c = 4
 
     class Node:
+        id_iter = itertools.count()
+
         def __init__(self, label, logprob, state, parent):
             self.value = 0  # total reward obtainable from node
             self.prob = exp(logprob)  # necessary for P-UCB calculation
@@ -44,7 +47,8 @@ def main():
             self._parent = parent
             self.visits = 0
             ### attributes for graph visualisation
-            self.label = label  # token label text
+            self.id = next(self.id_iter)
+            self.label = repr(label)  # token label text
             self.p_ucb = 0  # last calculated p_ucb value
 
         def backprop(self, value):
@@ -53,6 +57,16 @@ def main():
                 self.value = value
                 if self._parent is not None:
                     self._parent.backprop(value)
+
+    class NodeEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, Node):
+                cpy = copy.copy(obj)
+                del cpy._parent
+                del cpy._children
+                return vars(cpy)
+            # Let the base class default method raise the TypeError
+            return json.JSONEncoder.default(self, obj)
 
     # Implements P-UCB heuristic as defined in https://arxiv.org/pdf/2303.05510.pdf#subsection.D.1
     # P-UCB-SELECT(s, c) = argmax_a P-UCB(s, a)
@@ -131,12 +145,26 @@ def main():
         num_rollouts = max_rollouts
         root = Node("<PD>", log(1), prompt, None)
         test_times = []
+        # graph snapshots for web visualisation
+        nodes, edges = {root.id: root}, {}
+        graph_dict = {}
         for i in range(max_rollouts):
+            graph_dict[i] = {
+                "selectedNodes": [root.id],
+                "state": "",
+                "completion": "",
+                "reward": 0,
+                "task_id": task_id,
+            }
             curr_node = root
             curr_node.visits += 1
             # selection
             while len(curr_node._children) > 0:
+                for child in curr_node._children:
+                    nodes[child.id] = child
+                    edges[(curr_node.id, child.id)] = True
                 curr_node = p_ucb_select(curr_node, curr_node._children)
+                graph_dict[i]["selectedNodes"].append(curr_node.id)
                 curr_node.visits += 1
 
             # expansion
@@ -146,6 +174,9 @@ def main():
                 for (token, logprob) in tokens
             ]
             curr_node._children = child_nodes
+            for child in child_nodes:
+                nodes[child.id] = child
+                edges[(curr_node.id, child.id)] = True
 
             # evaluation
             reward = match_cached_programs(curr_node.state, program_dict)
@@ -158,6 +189,11 @@ def main():
                 test_end = time.perf_counter()
                 test_times.append(test_end - test_start)
                 program_dict[generated_program] = reward
+                graph_dict[i]["state"] = curr_node.state.replace(prompt, "")
+                graph_dict[i]["completion"] = completion
+                graph_dict[i]["reward"] = reward
+            graph_dict[i]["nodes"] = list(nodes.values())
+            graph_dict[i]["edges"] = list(edges.keys())
 
             # backprop
             curr_node.backprop(reward)
@@ -186,6 +222,8 @@ def main():
         render_graphviz_tree(
             root, filename=f"svgviz/tree_{task_id.replace('/', '_')}", view=False
         )
+        with open(f"web/public/graph_{task_id.replace('/', '_')}.json", "w") as f:
+            json.dump(graph_dict, f, cls=NodeEncoder)
 
     end = time.perf_counter()
     print(f"Total elapsed time: {(end - start):.4f}s\n")
--- src/visualise.py
+++ src/visualise.py
@@ -4,15 +4,12 @@ from graphviz import Digraph, escape
 def trace_graph(root):
     # builds a set of all nodes and edges in a graph
     nodes, edges = set(), set()
-    i = 0  # node id
 
     def build(v):
-        nonlocal i
         if v not in nodes:
-            nodes.add((v, i))
-            i += 1
+            nodes.add(v)
             for child in v._children:
-                edges.add((child, v))
+                edges.add((child.id, v.id))
                 build(child)
 
     build(root)
@@ -22,25 +19,25 @@ def trace_graph(root):
 def draw_graphviz_dot(root):
     dot = Digraph(format="svg", graph_attr={"rankdir": "TB"})  # TB = top to bottom
     nodes, edges = trace_graph(root)
-    for n, i in nodes:
-        uid = str(id(n))
+    for n in nodes:
+        uid = str(n.id)
         # HACK: quick-fix to escape special '<' & '>' chars during graphviz label processing
         escaped_n_label = (
-            escape(repr(n.label))
+            escape(n.label)
             .replace(">", "\>")
             .replace("<", "\<")
             .replace("}", "\}")
             .replace("{", "\{")
         )
-        label = f"{{ {i} | {escaped_n_label} | value {n.value:.4f} | p_ucb {n.p_ucb:.4f} | visits {n.visits} | prob {n.prob:.4f} }}"
+        label = f"{{ {uid} | {escaped_n_label} | value {n.value:.4f} | p_ucb {n.p_ucb:.4f} | visits {n.visits} | prob {n.prob:.4f} }}"
         # for any value in the graph, create a rectangular ('record') node for it
         dot.node(
             name=uid,
             label=label,
             shape="record",
         )
-    for n1, n2 in edges:
-        dot.edge(str(id(n2)), str(id(n1)))
+    for id1, id2 in edges:
+        dot.edge(str(id2), str(id1))
     return dot
 
 
